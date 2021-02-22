07375b8341dadaadb11d9d989aa0ef66bf8cc2e3
aymuos <sparkacco@gmail.com>
2021-02-22 19:51:20 +0530

    Functionalities implementation done 98%.

--- VisualizeMarks.php
+++ VisualizeMarks.php
@@ -5,106 +5,12 @@
     <title>Estimate GATE 2016 Rank | See How Others Performed | Gateoverflow.in</title>
     <!-- http://www.webgeekly.com/tutorials/jquery/how-to-switch-css-files-on-the-fly-using-jquery/ -->
     <link id="dynamic-css" rel="stylesheet" type="text/css" href="visualizeMarks-dark.css">
-    <link rel="stylesheet" type="text/css" href="subject_select/css/select.css">
+    <!-- <link rel="stylesheet" type="text/css" href="subject_select/css/select.css"> -->
 </head>
 <body>
     <div align="center" id="top-float">
         <table width="80%">
 
-
-            <tr>
-                <td> 
-                    <div>
-            
-                        <p>SELECT SUBJECT FROM THE LIST </p>
-                        <div class="subject-chips-container">
-                            <div class="chip">
-                                Computer Organisation
-                                <svg
-                                            class="clear-icon icon"
-                                            width="24"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            style="display: none;"
-                                        >
-                                        <path
-                                            d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
-                                            fill="currentColor"
-                                        />
-                                </svg>
-                            </div>
-                        </div>
-                        
-                        <div class="custom-select-wrapper">
-                            <div class="custom-select">
-                                <span class="custom-select-trigger">
-                                    Choose Subject
-                                </span> 
-                                <div class="wide custom-options">
-                                
-                                    <form class="search-container">
-                                        <input type="text" id="search-bar" placeholder="search subject"/>
-                                        <svg
-                                            class="search-icon icon"
-                                            width="24"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path
-                                                fill-rule="evenodd"
-                                                clip-rule="evenodd"
-                                                d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z"
-                                                fill="currentColor"
-                                            />
-                                        </svg>
-                                        <svg
-                                            class="clear-icon icon"
-                                            width="24"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            style="display: none;"
-                                        >
-                                            <path
-                                                d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
-                                                fill="currentColor"
-                                            />
-                                        </svg>
-                                    </form>
-                                    <ul class="select-menu">
-    
-    
-    
-                                        <!-- <li data-value="Physics">
-                                            <input type="checkbox"/>
-                                            <div>Physics</div>
-                                        </li>
-                                        <li data-value="Maths"><input type="checkbox"/><div>Maths</div></li>
-                                        <li data-value="iwhiw"><input type="checkbox"/><div>ljeorpibife</div></li>
-                                        <li data-value="tybfibi"><input type="checkbox"/><div>ooibboa</div></li>
-                                        <li data-value="iwnbgibc"><input type="checkbox"/><div>pnbcaahw</div></li>
-                                        <li data-value="lllpefincc"><input type="checkbox"/><div>wpjnnn</div></li>
-                                        <li data-value="poninas"><input type="checkbox"/><div>ojoff oihfpoj</div></li>
-                                        <li data-value="OUnit"><input type="checkbox"/>Operation Unit</li>
-                                        <li data-value="dogge"><input type="checkbox"/>ShibuDoggie</li>
-                                        <li data-value="cat"><input type="checkbox"/>Meowm</li>
-                                        <li data-value="pontiac"><input type="checkbox"/>GT2 Firebird TransAM</li>
-                                        <li data-value=cse"><input type="checkbox"/>Computer Science</li>
-                                        <li data-value="ceramics"><input type="checkbox"/>Ceramic Technology</li>
-                                        <li data-value="heattransfer"><input type="checkbox"/>HotAtoCOldB</li> -->
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <!-- <hr /> -->
-                </td>
-            </tr>
             <tr>
                 <td><table class="top-tables">
                     <tr>
@@ -158,6 +64,91 @@
                 </table></td>
             </tr>
         </table>
+        <p class="custom-select-label">Select subjects from the list: </p>
+        <div class="subject-select">
+            <div class="subject-chips-container">
+                <!-- <div class="chip">
+                    Computer Organisation
+                    <svg
+                                class="clear-icon icon"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                            <path
+                                d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+                                fill="currentColor"
+                            />
+                    </svg>
+                </div> -->
+            </div>
+            <div class="custom-select-wrapper">
+                <div class="custom-select">
+                    <span class="custom-select-trigger">
+                        Choose Subject
+                    </span> 
+                    <div class="wide custom-options">
+                    
+                        <form class="search-container">
+                            <input type="text" id="search-bar" placeholder="search subject"/>
+                            <svg
+                                class="search-icon icon"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    clip-rule="evenodd"
+                                    d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z"
+                                    fill="currentColor"
+                                />
+                            </svg>
+                            <svg
+                                class="clear-icon icon"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                                style="display: none;"
+                            >
+                                <path
+                                    d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+                                    fill="currentColor"
+                                />
+                            </svg>
+                        </form>
+                        <ul class="select-menu">
+
+
+
+                            <!-- <li data-value="Physics">
+                                <input type="checkbox"/>
+                                <div>Physics</div>
+                            </li>
+                            <li data-value="Maths"><input type="checkbox"/><div>Maths</div></li>
+                            <li data-value="iwhiw"><input type="checkbox"/><div>ljeorpibife</div></li>
+                            <li data-value="tybfibi"><input type="checkbox"/><div>ooibboa</div></li>
+                            <li data-value="iwnbgibc"><input type="checkbox"/><div>pnbcaahw</div></li>
+                            <li data-value="lllpefincc"><input type="checkbox"/><div>wpjnnn</div></li>
+                            <li data-value="poninas"><input type="checkbox"/><div>ojoff oihfpoj</div></li>
+                            <li data-value="OUnit"><input type="checkbox"/>Operation Unit</li>
+                            <li data-value="dogge"><input type="checkbox"/>ShibuDoggie</li>
+                            <li data-value="cat"><input type="checkbox"/>Meowm</li>
+                            <li data-value="pontiac"><input type="checkbox"/>GT2 Firebird TransAM</li>
+                            <li data-value=cse"><input type="checkbox"/>Computer Science</li>
+                            <li data-value="ceramics"><input type="checkbox"/>Ceramic Technology</li>
+                            <li data-value="heattransfer"><input type="checkbox"/>HotAtoCOldB</li> -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div><hr />
     <div align="center">
         <a href="ScoreToColleges.php" id="colleges">Click to <b>View list of IITs and NITs. See which colleges you might get.</b></a>
--- subject_select/js/select.js
+++ subject_select/js/select.js
@@ -1,8 +1,9 @@
 const menu = document.querySelector(".select-menu")
 const search_bar = document.querySelector("#search-bar")
-const search_icon = document.querySelector(".search-icon")
-const clear_icon = document.querySelector(".clear-icon")
+const search_icon = document.querySelector(".search-container .search-icon")
+const clear_icon = document.querySelector(".search-container .clear-icon")
 const select_trigger = document.querySelector(".custom-select-trigger")
+const chip_container = document.querySelector(".subject-chips-container")
 const html = document.querySelector("html")
 let cat = {}
 let data = {};
@@ -23,6 +24,18 @@ clear_icon.addEventListener("click",(e)=>{
      updateIcon();
      updateUIList(data);
 })
+chip_container.addEventListener("click",(e)=>{
+     // cat = e;
+    if(e.target.closest("svg")){
+          // cat=e;
+          let chip = e.target.closest(".chip");
+          let index = chip.getAttribute("data-list-index")
+          data[index].isChecked = false
+          let li = document.querySelector(`li[data-id='${data[index].id}']`)
+          li.querySelector("input").checked = false;
+          updateUIChips()
+    } 
+})
 
 const updateIcon = ()=>{
      if(search_bar.value==""){
@@ -34,7 +47,39 @@ const updateIcon = ()=>{
           clear_icon.style.display="";
      }
 }
-
+const updateUIChips = ()=>{
+     chip_container.innerHTML=""
+     let count=0;
+     data.forEach((elem,index)=>{
+          if(elem.isChecked){
+               chip_container.innerHTML+=`
+               <div class="chip" data-id="${elem.id}" data-list-index="${index}">
+                    ${elem.name}
+                    <svg
+                              class="clear-icon icon"
+                              width="19"
+                              height="19"
+                              viewBox="0 0 19 19"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                         >
+                         <path
+                              d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+                              fill="currentColor"
+                         />
+                    </svg>
+               </div>
+               `
+               count++;
+          }
+     })
+     if(count==0){
+          chip_container.style.display = "none";
+     }
+     else{
+          chip_container.style.display = "block";
+     }
+}
 const updateUIList = (data)=>{
      menu.innerHTML = "";
      data.forEach((element,index) => {
@@ -65,7 +110,9 @@ menu.addEventListener("click",(e)=>{
         let inp = li.querySelector("input")
         inp.checked = !inp.checked
      }
-     data[li.getAttribute("data-list-index")].isChecked = !data[li.getAttribute("data-list-index")].isChecked
+     let element = data.filter((elem)=> elem.id==li.getAttribute("data-id"))[0];
+     element.isChecked = !element.isChecked;
+     updateUIChips();
 })
 
 search_bar.addEventListener("keyup",()=>{
